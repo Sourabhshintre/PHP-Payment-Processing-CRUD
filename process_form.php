e1d5386b0fe09d33bb0719f2068c9f88660f8918
Sourabh Shintre <96312514+Sourabhshintre@users.noreply.github.com>
2024-07-27 15:33:16 +0530

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,13 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Replace with your MySQL username
-$password = ""; // Replace with your MySQL password
+$username = "root";
+$password = ""; 
 $dbname = "reso";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
